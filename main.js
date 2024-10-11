d3d8bb978df14c485542870883af53118e953beb
AndresCecconi <ancecconi15@gmail.com>
2024-10-11 17:58:23 -0300

    formulario en home

---
+++ main.js
@@ -0,0 +1,44 @@
+function validateCaptcha() {
+    var response = grecaptcha.getResponse();
+    if (response.length === 0) {
+        alert("Por favor, completa el CAPTCHA.");
+        return false; 
+    }
+    return true;
+}
+
+function allowOnlyNumbers(event) {
+    const characterCode = (event.which) ? event.which : event.keyCode;
+    // Si el caracter ingresado es un numero o la tecla de borrar devuelve true
+    if ((characterCode >= 48 && characterCode <= 57) || characterCode === 8) {
+        return true;
+    }
+    return false; //sino false y no permite la entrada
+}
+
+
+function validateContactForm() {
+    const emailInputValue = document.getElementById("email").value;
+    if (!emailInputValue.includes("@")) {
+        alert("Por favor, ingresa un correo electrónico válido");
+        return false; // Detiene el envío del formulario
+    }
+
+    const phoneInputValue = document.getElementById("phone").value;
+    if (!/^\d+$/.test(phoneInputValue)) {
+        alert("El número de contacto debe contener solo números");
+        return false; // Detiene el envío del formulario
+    }
+
+    return true; // Permite el envío del formulario
+}
+
+function resetForm() {
+    document.getElementById("form").reset();
+}
+//LLama a la funcion reset despues del submit
+document.getElementById("form").addEventListener("submit", function(event) {
+    if (validateContactForm(event)) {
+        resetForm();
+    }
+});
